ad4b6fd017a75e84e1f78aff0a3da5c621cebb3b
Alexander Groß <agross@therightstuff.de>
2023-08-20 09:18:11 +0200

    Add release script

---
+++ min-brightness.sh
@@ -0,0 +1,15 @@
+#!/system/bin/sh
+
+# This script checks the current brightness ensures a minimum brightness of 15.
+# This prevents the green screen tint if the brightness is low.
+
+BRIGHTNESS_FILE=/sys/class/backlight/panel0-backlight/brightness
+BRIGHTNESS="$(cat "$BRIGHTNESS_FILE")"
+
+echo "Current brightness: $BRIGHTNESS"
+
+if [ "$BRIGHTNESS" -lt 15 ]; then
+  echo 15 > "$BRIGHTNESS_FILE"
+fi
+
+exit 0
---
+++ service.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+# Do NOT assume where your module will be located. ALWAYS use $MODDIR if you
+# need to know where this script and module is placed. This will make sure your
+# module will work if Magisk changes it's mount point in the future.
+MODDIR="${0%/*}"
+
+BRIGHTNESS_FILE=/sys/class/backlight/panel0-backlight/brightness
+BRIGHTNESS_SCRIPT="$MODDIR/min-brightness.sh"
+
+until [ "$(getprop sys.boot_completed)" = 1 ] && \
+      [ "$(getprop dev.bootcomplete)" = 1 ] && \
+      [ "$(getprop service.bootanim.exit)" = 1 ] && \
+      [ "$(getprop init.svc.bootanim)" = stopped ]; do
+  sleep 5
+done
+
+inotifyd "$BRIGHTNESS_SCRIPT" "$BRIGHTNESS_FILE:/w"
+
+exit 0
